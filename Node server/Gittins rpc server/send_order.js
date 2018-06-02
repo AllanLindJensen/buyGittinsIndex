4183e7e8cf0f071f70c4cb1814990bfbfe5cfb39
AllanLindJensen <allanl.jensen@gmail.com>
2018-06-02 17:11:13 +0200

    Invoice: changed from <p> to <textarea> QRcode added: button and code. works Copy to clipboard. button and code. works. Next step: Check every second, whether the bill has been paid. Then: Change gittins-server.js to give new bill and check its payment.

--- Node server/Gittins rpc server/send_order.js
+++ Node server/Gittins rpc server/send_order.js
@@ -1,15 +1,20 @@
 var grpc = require('grpc');
 var receive_order = grpc.load('../Proto files/buyGittinsIndex.proto').buygittinsindex;
 function main() {
-  var client = new receive_order.BuyGittinsIndex('176.21.113.33:14203',
+  var client = new receive_order.BuyGittinsIndex('127.0.0.1:14203',
 	grpc.credentials.createInsecure());
   client.orderGittinsIndex({
       discount: 90,
       successes: 1,
       failures: 0
     }, function(err, response) {
-    console.log('Please pay: ' + response.billText);
-    console.log('r_hash: ' + response.r_hash);
+    if (err != undefined)
+    {
+      console.log('ERROR: '+err)
+    } else {
+      console.log('Please pay: ' + response.billText);
+      console.log('r_hash: ' + response.r_hash);
+    }
   })
 }
 
