4b6e4bf7ddb9c03204e1036b16a757ad4feef1e5
AllanLindJensen <allanl.jensen@gmail.com>
2018-06-03 14:28:29 +0200

    yuuhuu

--- Node server/Gittins rpc server/check_bill.js
+++ Node server/Gittins rpc server/check_bill.js
@@ -2,19 +2,20 @@ var grpc = require('grpc');
 var fs = require('fs');
 var lndCert = fs.readFileSync("/home/allan/.lnd/tls.cert");
 var credentials = grpc.credentials.createSsl(lndCert);
-var lnrpcDescriptor = grpc.load('lnd_rpc.proto');
+var lnrpcDescriptor = grpc.load('../Proto files/lnd_rpc.proto');
 var lnrpc = lnrpcDescriptor.lnrpc;
 var lightning = new lnrpc.Lightning('localhost:10009', credentials); 
-var unlocker = new lnrpc.WalletUnlocker('localhost:10009', credentials);
 
 function main() {
   process.env.GRPC_SSL_CIPHER_SUITES = 'HIGH+ECDSA';
-
-  lightning.getInfo({}, function(err, response) {
+  lightning.lookupInvoice({
+       r_hash: process.argv[2]
+    },
+    function(err, response) {
     if (err != undefined) {
       console.log("ERR: "+err);
     }
-    console.log("INFO: " + response.block_height);
+    console.log("Paid: " + response.settled);
   })
 }
 
